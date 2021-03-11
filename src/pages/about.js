dc522550260d716df301400a190c2f5741dc8dcc
@farissahman1 <farissahman1@gmail.com>
2021-03-11 22:44:24 +0100

    it is late

---
+++ src/pages/about.js
@@ -0,0 +1,26 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const About = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae
+          saepe voluptatibus pariatur repellendus ex fugit molestias temporibus
+          harum omnis aut, quas ea repellat fuga, quam fugiat ab, non suscipit
+          libero?
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae
+          saepe voluptatibus pariatur repellendus ex fugit molestias temporibus
+          harum omnis aut, quas ea repellat fuga, quam fugiat ab, non suscipit
+          libero?
+        </p>
+      </div>
+    </Layout>
+  )
+}
+
+export default About
